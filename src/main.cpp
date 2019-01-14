368d8b646a05bcc35e66d95a808a2a6ffa9fb84c
Chris-1101 <chrismb.noreply@gmail.com>
2019-01-14 21:16:10 +0000

    :white_check_mark: Print results

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include "assembler.hpp"
+using namespace std;
+
+void print_result(vector<string> instructions)
+{
+  map<string, int> programme = assembler(instructions);
+
+  for (auto i = programme.begin(); i != programme.end(); ++i)
+  {
+    cout << i->first << ": " << i->second << endl;
+  }
+  cout << std::endl;
+}
+
+int main(void)
+{
+  print_result({ "mov a 5", "inc a", "dec a", "dec a", "jnz a -1", "inc a" });
+  print_result({ "mov a -10", "mov b a", "inc a", "dec b", "jnz a -2" });
+  print_result({ "mov d 100", "dec d", "mov b d", "jnz b -2", "inc d", "mov a d", "jnz 5 10", "mov c a" });
+
+  return 0;
+}
